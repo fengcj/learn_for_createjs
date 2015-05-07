b21049d48ccd1c692c89b6fd98bcd66996283685
fcj <fcjljy@gmail.com>
2015-05-07 18:11:41 +0800

    add eventListener for price

--- AddImageOnCanvas/app.js
+++ AddImageOnCanvas/app.js
@@ -10,14 +10,26 @@ stage.x = 150;*/
 var container = new createjs.Container();
 stage.addChild(container);
 
-var background, title, statement, price;
+var background, title, statement, price,increaseButon,decreaseButton;
 
 
-var backgroundImage = new Image();
+/*var backgroundImage = new Image();
 backgroundImage.onload = function() {
 	addBackground();
 }
 backgroundImage.src = "./assets/images/background.PNG";
+
+
+
+
+var titleImage = new Image();
+titleImage.onload = function() {
+	addTitle();
+	addStatement();
+	addPrice();
+};
+titleImage.src = "./assets/images/title.png";*/
+
 
 function addBackground() {
 	background = new createjs.Bitmap("./assets/images/background.PNG");
@@ -31,14 +43,6 @@ function addBackground() {
 	container.addChild(background);
 }
 
-
-var titleImage = new Image();
-titleImage.onload = function() {
-	addTitle();
-	addStatement();
-	addPrice();
-};
-titleImage.src = "./assets/images/title.png";
 
 function addTitle() {
 	title = new createjs.Bitmap("./assets/images/title.png");
@@ -62,9 +66,71 @@ function addStatement() {
 function addPrice() {
 	price = new createjs.Text("2 $", "40px Arial", "#ff0000");
 	price.x = (stage.canvas.width - price.getBounds().width) / 2;
-	price.y = (title.getBounds().height);	
+	console.log("in addPrice " + title.getBounds());
+	price.y = title.getBounds().height;	
 	container.addChild(price);
 }
+
+function addIncreaseButtonAndDecreaseButton(){
+
+	var listener = function(event){
+	//	console.log(event.target);
+		var priceNum = Number(price.text.substr(0,price.text.length-1));
+	//	console.log(typeof priceNum);
+
+	//// TODO less hard core and priceNum may be some certain Number ,like 2 5 8 10
+		if(event.target.text === "+"){
+			price.text = priceNum + 1 + " $";
+		}else{
+			if(priceNum == 0){
+				price.text = 0 + " $"; 
+			}else{
+				price.text = priceNum - 1 + " $"; 
+			}
+			
+		}
+	}
+
+	increaseButon = new createjs.Text("+","40px Arial","#00ff00");
+	increaseButon.x = title.x;
+	increaseButon.y = title.getBounds().height;
+
+
+	decreaseButton = new createjs.Text("-","40px Arial","#0000ff");
+	decreaseButton.x = title.x + title.getBounds().width - decreaseButton.getBounds().width;
+	decreaseButton.y = title.getBounds().height;
+
+	increaseButon.addEventListener("click",listener);
+	decreaseButton.addEventListener("click",listener);
+
+	container.addChild(increaseButon);
+	container.addChild(decreaseButton);
+}
+
+function onPreLoadComplete(){
+	addBackground();
+	addTitle();
+	addStatement();
+	addPrice();
+	addIncreaseButtonAndDecreaseButton();
+}
+
+var preLoadManifest = [
+	{
+		id: "background",
+		src: "./assets/images/background.PNG"
+	},{
+		id: "title",
+		src: "./assets/images/title.png"		
+	}
+];
+
+
+var preLoader = new createjs.LoadQueue(false);  // must set to be false when just run the index.html
+preLoader.on("complete",onPreLoadComplete);
+preLoader.loadManifest(preLoadManifest);
+
+
 
 
 
